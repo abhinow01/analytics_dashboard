629f0abb51e83977b30392aafb3c74be2d322a68
abhinav khanna <kabhinav99@gmail.com>
2024-01-27 18:55:40 +0530

    added cards

--- src/DashBoardStatsGrid.jsx
+++ src/DashBoardStatsGrid.jsx
@@ -4,54 +4,79 @@ import { CiCircleQuestion } from 'react-icons/ci';
 function DashBoardStatsGrid() {
   return (
     <div className='flex flex-row'>
-    <div className='flex gap-4 w-full flex-col'>
-      <div className='bg-white rounded-sm p-4 flex-1 border border-gray-200 flex  flex-col'>
-        <div className='flex items-center justify-between '>
-          <div className='text-sm text-gray-400 font-light flex items-center justify-between'>
-            Online orders <CiCircleQuestion className='ml-1 text-sm' />
-          </div>
-          <div className='text-sm text-gray-400 font-light'>by today, 04:00 pm</div>
-        </div>
-        <div className='text-lg py-2 font-bold'>231</div>
-        {/* <a href='#'className="py-2 text-blue-500 text-sm font-light underline">View 13 orders</a> */}
-      </div>
-
-      <div className='bg-white rounded-sm p-4 flex-1 border border-gray-200 flex flex-col'>
+  <div className='flex gap-4  flex-col'>
+    <div className='bg-white rounded-sm  border border-gray-200 flex flex-col flex-1 px-4'>
       <div className='flex items-center justify-between'>
-          <div className='text-sm text-gray-400 font-light flex items-center justify-between'>
-            Amount Recieved <CiCircleQuestion className='ml-1 text-sm' />
-          </div>
-          <div className='text-sm text-gray-400 font-light'>by today, 04:00 pm</div>
+        <div className='text-sm text-gray-400 font-light flex items-center justify-between'>
+          <img src='./IconBox.png' />
         </div>
-        <div className='py-2 text-lg'>₹23,92,312.20</div>
+        <div><img src='./Group 3 (1).png'/></div>
       </div>
- </div>  
-
- <div className='flex flex-col gap-4'>
-      <div className='bg-white rounded-sm p-4 flex-1 border border-gray-200 flex  flex-col'>
-        <div className='flex items-center justify-between '>
-          <div className='text-sm text-gray-400 font-light flex items-center justify-between'>
-            Online orders <CiCircleQuestion className='ml-1 text-sm' />
-          </div>
-          <div className='text-sm text-gray-400 font-light'>by today, 04:00 pm</div>
-        </div>
-        <div className='text-lg py-2 font-bold'>231</div>
-        {/* <a href='#'className="py-2 text-blue-500 text-sm font-light underline">View 13 orders</a> */}
+      <div className='text-lg py-2 font-bold flex flex-col'>
+        <div className='text-sm font-light'>Total Refund</div>
+        <strong>231</strong>
       </div>
-
-      <div className='bg-white rounded-sm p-4 flex-1 border border-gray-200 flex flex-col'>
-      <div className='flex items-center justify-between'>
-          <div className='text-sm text-gray-400 font-light flex items-center justify-between'>
-            Amount Recieved <CiCircleQuestion className='ml-1 text-sm' />
-          </div>
-          <div className='text-sm text-gray-400 font-light'>by today, 04:00 pm</div>
-        </div>
-        <div className='py-2 text-lg'>₹23,92,312.20</div>
+      <div className='flex flex-row justify-between'>
+        <img src='./Up.png' />
+        <div className='text-sm font-light text-gray-400'>v.s previous month</div>
       </div>
-
     </div>
 
+    <div className='bg-white rounded-sm p-2 border border-gray-200 flex flex-col flex-1'>
+      <div className='flex items-center justify-between'>
+        <div className='text-sm text-gray-400 font-light flex items-center justify-between'>
+          <img src='./IconBox.png' />
+        </div>
+        <div><img src='./Group 3 (1).png'/></div>
+      </div>
+      <div className='text-lg py-2 font-bold flex flex-col'>
+        <div className='text-sm font-light'>Total Refund</div>
+        <strong>231</strong>
+      </div>
+      <div className='flex flex-row justify-between'>
+        <img src='./Up.png' />
+        <div className='text-sm font-light text-gray-400'>v.s previous month</div>
+      </div>
     </div>
+  </div>
+
+  <div className='flex flex-col gap-4'>
+    <div className='bg-white rounded-sm p-2 border border-gray-200 flex flex-col'>
+      <div className='flex items-center justify-between'>
+        <div className='text-sm text-gray-400 font-light flex items-center justify-between'>
+          <img src='./IconBox.png' />
+        </div>
+        <div><img src='./Group 3 (1).png'/></div>
+      </div>
+      <div className='text-lg py-2 font-bold flex flex-col'>
+        <div className='text-sm font-light'>Total Refund</div>
+        <strong>231</strong>
+      </div>
+      <div className='flex flex-row justify-between'>
+        <img src='./Up.png' />
+        <div className='text-sm font-light text-gray-400'>v.s previous month</div>
+      </div>
+    </div>
+
+    <div className='bg-white rounded-sm p-2 border border-gray-200 flex flex-col'>
+      <div className='flex items-center justify-between'>
+        <div className='text-sm text-gray-400 font-light flex items-center justify-between'>
+          <img src='./IconBox.png' />
+        </div>
+        <div><img src='./Group 3 (1).png'/></div>
+      </div>
+      <div className='text-lg py-2 font-bold flex flex-col'>
+        <div className='text-sm font-light'>Total Refund</div>
+        <strong>231</strong>
+      </div>
+      <div className='flex flex-row justify-between'>
+        <img src='./Up.png' />
+        <div className='text-sm font-light text-gray-400'>v.s previous month</div>
+      </div>
+    </div>
+  </div>
+</div>
+
   );
 }
 
